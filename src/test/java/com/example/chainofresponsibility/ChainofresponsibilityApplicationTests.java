4e32ab8d99cafd342d5d3c7d70f7009ec9124ca8
R7000P <1208370852@qq.com>
2022-08-22 16:47:06 +0800

    下订单完成

--- src/test/java/com/example/chainofresponsibility/ChainofresponsibilityApplicationTests.java
+++ src/test/java/com/example/chainofresponsibility/ChainofresponsibilityApplicationTests.java
@@ -17,14 +17,16 @@ class ChainofresponsibilityApplicationTests {
     void contextLoads() {
 
         //可以从spring中获取，这个可以固定死注入spring，每个pipeline对应一个业务抽象，例如下订单
+        //此处的pipeline是有序的，相当于下订单中执行的链条是有序的
         FilterChainPipeline filterChainPipeline = new FilterChainPipeline();
-        filterChainPipeline.addFilter(new CustomerInfoCheckFilter());
         filterChainPipeline.addFilter(new SaveExchangeLogFilter());
+        filterChainPipeline.addFilter(new CustomerInfoCheckFilter());
 
         //创建selector,可以创建不同的selector，发票selector或者结算selecotr，每个selector里面可以放不同的filter
+        //selecotr无序，也就是最终执行时，拿到selector中所有filter与pipeline进行比对，从而执行跳链功能
         LocalListBasedFilterSelector localListBasedFilterSelector = new LocalListBasedFilterSelector();
-        localListBasedFilterSelector.addFilter(CustomerInfoCheckFilter.class.getSimpleName());
         localListBasedFilterSelector.addFilter(SaveExchangeLogFilter.class.getSimpleName());
+        localListBasedFilterSelector.addFilter(CustomerInfoCheckFilter.class.getSimpleName());
 
         //模拟业务开始调用
         //获取服务传过来的model,此处为订单服务，也就是对应的订单表对象
